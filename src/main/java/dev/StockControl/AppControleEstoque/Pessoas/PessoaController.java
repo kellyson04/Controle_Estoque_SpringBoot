1b1401974837cd0318b3a747397bb89b445009c0
Kellyson <gilsonvigilante@outlook.com>
2026-03-09 00:51:51 -0300

    feat:adicionado os mockpoints no controller para testar o CRUD.

--- src/main/java/dev/StockControl/AppControleEstoque/Pessoas/PessoaController.java
+++ src/main/java/dev/StockControl/AppControleEstoque/Pessoas/PessoaController.java
@@ -2,10 +2,47 @@ package dev.StockControl.AppControleEstoque.Pessoas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/pessoa")
 public class PessoaController {
+    private PessoaService pessoaService;
+
+    public PessoaController(PessoaService pessoaService) {
+        this.pessoaService = pessoaService;
+    }
+
+    // C
+    @PostMapping("/criar")
+    public PessoaModel criarPessoas (@RequestBody PessoaModel pessoaModel) {
+        return pessoaService.criarPessoa(pessoaModel);
+    }
+
+    //R
+    @GetMapping("/listar")
+    public List<PessoaModel> listaPessoas () {
+        return pessoaService.listaDePessoas();
+    }
+
+    //R
+    @GetMapping("/listar/{id}")
+    public PessoaModel pessoaPorId (@PathVariable Long id) {
+        return pessoaService.listarPessoaPorID(id);
+    }
+
+    //U
+    @PutMapping("/atualizar/{id}")
+    public PessoaModel atualizarPessoas (@PathVariable Long id, @RequestBody PessoaModel alterarPessoa) {
+        return pessoaService.alterarPessoa(id,alterarPessoa);
+    }
 
 
+    //D
+    @DeleteMapping("/deletar/{id}")
+    public void deletarPessoas (@PathVariable Long id) {
+
+        pessoaService.deletarPessoa(id);
+    }
 
 }
