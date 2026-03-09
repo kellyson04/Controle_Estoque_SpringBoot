e71b4ba6b7ebf1b7b320cc8a42911897cf330c1f
Kellyson <gilsonvigilante@outlook.com>
2026-03-09 00:50:46 -0300

    feat:adicionada logica crud no services de Pessoas.

--- src/main/java/dev/StockControl/AppControleEstoque/Pessoas/PessoaService.java
+++ src/main/java/dev/StockControl/AppControleEstoque/Pessoas/PessoaService.java
@@ -1,4 +1,45 @@
 package dev.StockControl.AppControleEstoque.Pessoas;
 
+import dev.StockControl.AppControleEstoque.Produtos.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PessoaService {
+
+    private PessoaRepository pessoaRepository;
+
+    public PessoaService(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    public PessoaModel criarPessoa (PessoaModel pessoaModel) {
+        return pessoaRepository.save(pessoaModel);
+    }
+
+    public List<PessoaModel> listaDePessoas () {
+        return pessoaRepository.findAll();
+    }
+
+    public PessoaModel listarPessoaPorID(Long id) {
+        return pessoaRepository.findById(id).orElse(null);
+    }
+
+    public PessoaModel alterarPessoa(Long id,PessoaModel novaPessoa) {
+        PessoaModel pessoaPraMudar = pessoaRepository.findById(id).orElse(null);
+
+        pessoaPraMudar.setNome(novaPessoa.getNome());
+        pessoaPraMudar.setEndereco(novaPessoa.getEndereco());
+
+        return pessoaRepository.save(pessoaPraMudar);
+    }
+
+    public void deletarPessoa (Long id) {
+        PessoaModel pessoaPraDeletar = pessoaRepository.findById(id).orElseThrow(() -> new RuntimeException("Pessoa não encontrada!."));
+        pessoaRepository.delete(pessoaPraDeletar);
+    }
+
 }
